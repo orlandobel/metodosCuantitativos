93c13363d9296e349c51b2ce3022eb01f757dd81
HectorGG <hagalvangarcia@gmail.com>
2020-03-01 00:43:44 -0600

    Inverso

--- metodo_grafico/lib/classes/Funcion.dart
+++ metodo_grafico/lib/classes/Funcion.dart
@@ -1,30 +1,29 @@
 import 'package:math_expressions/math_expressions.dart';
 
 class Funcion {
-  List<num> x;
-  List<num> y;
-  List<num> solucion;
+  List<num> x,y,solucion;
   Number m,b;
-  Expression exp;
-  Variable equis;
+  Expression exp,inverso;
+  Variable equis,ie;
+ 
   Funcion(List<double> funcion, bool fo) {
     x=new List<num>();
     y=new List<num>();
+    solucion=new List<num>();
     equis = new Variable('x');
-    m= new Number(-funcion[0]/funcion[1]);
+    m=new Number(-funcion[0]/funcion[1]);
     if(fo){
       b = new Number(0);
+      Number aux1=new Number(funcion[0]);
+      Number aux2=new Number(funcion[1]);
+      ie=new Variable('y');
+      exp=(aux1*equis)+(aux2*ie);
     }else{
       b= new Number(funcion[2]/funcion[1]);
+      exp=m*equis+b;
+      inverso=(equis-b)/m;
     }
-    exp=m*equis+b;
-
-    //exp=(c-(a*x))/b;
-    
-    //exp.evaluate(EvaluationType.REAL, context);
-    
-
-    
+  
   }
 
 }
--- metodo_grafico/lib/classes/Problema.dart
+++ metodo_grafico/lib/classes/Problema.dart
@@ -1,14 +1,11 @@
 import 'package:math_expressions/math_expressions.dart';
 import 'package:metodo_grafico/classes/Funcion.dart';
-/*
-***** author Hector
-  */
+
 class Problema {
   Funcion _funcionObjetivo;
   num _numRestricciones;
   bool max;
   List<Funcion> _restricciones; // Funciones originales de las restricciones
-  //List<Funcion> _funcionRestricciones; // Funicones despejadas de las restricciones
 
   Problema(Funcion fo, num res,List<Funcion> fr,bool op) {
     _funcionObjetivo = fo; 
@@ -22,6 +19,15 @@ class Problema {
   // Guardar cada par ordenado en las listas 'x' 'y' de las funciones de restricción
   void buscarIntersecciones(){
     for(int i =0;i<_restricciones.length-1;i++){
+        Number mayra = new Number(0);
+        ContextModel cm = new ContextModel();
+        cm.bindVariable(_restricciones[i].equis, mayra);
+
+        double s=_restricciones[i].exp.evaluate(EvaluationType.REAL, cm);
+
+        _restricciones[i].x.add(0.0);
+        _restricciones[i].y.add(s);
+        
       for(int j=i+1;j<_restricciones.length;j++){
         double dividendo=_restricciones[j].b.getConstantValue()-_restricciones[i].b.getConstantValue();
         double divisor= _restricciones[i].m.getConstantValue()-_restricciones[j].m.getConstantValue();
@@ -38,9 +44,26 @@ class Problema {
         
         _restricciones[j].x.add(aux.getConstantValue());
         _restricciones[j].y.add(s);
+
+        Number aux1=new Number(s);
+        ContextModel cm1 = new ContextModel();
+        cm1.bindVariable(_funcionObjetivo.equis, aux);
+        cm1.bindVariable(_funcionObjetivo.ie, aux1);
+        double s2=_funcionObjetivo.exp.evaluate(EvaluationType.REAL, cm1);
+        _restricciones[i].solucion.add(s2);
+        _restricciones[j].solucion.add(s2);
+
+
       }
-      //Pantalla_de_espera_mientras_voy_a_mear.destroy;
-      }
+    
+      Number mayra1 = new Number(0);
+      ContextModel cm3 = new ContextModel();
+      cm3.bindVariable(_restricciones[i].equis, mayra1);
+      double s1=_restricciones[i].inverso.evaluate(EvaluationType.REAL, cm);
+      _restricciones[i].y.add(mayra.getConstantValue());
+      _restricciones[i].x.add(s1);
+        
+    }
   }
   
 }
